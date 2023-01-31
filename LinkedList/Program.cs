0800b88fe2646c696ac080040318e5d16da0ad33
vaibhav7007 <58113732+vaibhav7007@users.noreply.github.com>
2023-02-01 03:13:40 +0530

    Update Program.cs

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -9,9 +9,11 @@ namespace DataStructure_LinkedList
             Console.WriteLine("Welcome to the data structure linked list");
             //UC1 Simple LinkedList
             LinkedList list = new LinkedList();
-            list.Insert(56);
-            list.Insert(30);
+            //UC2 adding elements to linked list
             list.Insert(70);
+            list.Insert(30);
+            list.Insert(56);
+            list.Display();
         }
     }
 }
